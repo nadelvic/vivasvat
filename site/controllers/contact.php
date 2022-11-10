c8f85fe8d049583e078ecd7347c124e46583968c
Nadelvic <nathan.delavictoire@gmail.com>
2022-11-10 14:44:30 +0100

    updating controller

--- site/controllers/contact.php
+++ site/controllers/contact.php
@@ -1,21 +1,97 @@
 <?php
+
+
+
+function validate_rechapcha($response){
+	// Verifying the user's response (https://developers.google.com/recaptcha/docs/verify)
+	$verifyURL = 'https://www.google.com/recaptcha/api/siteverify';
+    echo "<pre>".$response."</pre>";
+	
+	// Collect and build POST data
+	$post_data = http_build_query(
+		array(
+			'secret' => '',
+			'response' => $response,
+			'remoteip' => (isset($_SERVER["HTTP_CF_CONNECTING_IP"]) ? $_SERVER["HTTP_CF_CONNECTING_IP"] : $_SERVER['REMOTE_ADDR'])
+		)
+	);
+	
+	// Send data on the best possible way
+	if(function_exists('curl_init') && function_exists('curl_setopt') && function_exists('curl_exec')) {
+		// Use cURL to get data 10x faster than using file_get_contents or other methods
+		$ch =  curl_init($verifyURL);
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+			curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-type: application/x-www-form-urlencoded'));
+			$response = curl_exec($ch);
+		curl_close($ch);
+	} else {
+		// If server not have active cURL module, use file_get_contents
+		$opts = array('http' =>
+			array(
+				'method'  => 'POST',
+				'header'  => 'Content-type: application/x-www-form-urlencoded',
+				'content' => $post_data
+			)
+		);
+		$context  = stream_context_create($opts);
+		$response = file_get_contents($verifyURL, false, $context);
+	}
+
+	// Verify all reponses and avoid PHP errors
+	if($response) {
+       
+		$result = json_decode($response);
+		if ($result->success===true) {
+        
+			return true;
+		} else {
+			return $result;
+		}
+	}
+
+	// Dead end
+	return false; 
+}
+
+
+
 return function($kirby, $pages, $page) {
 
     $alert = null;
 
     if($kirby->request()->is('POST') && get('submit')) {
 
+        
+
         // check the honeypot
         /*if(empty(get('website')) === false) {
             go($page->url());
             exit;
         }*/
+    
 
+        // check the captcha
+        
+        $captchaResponse = get('g-recaptcha-response');
+   
+        if (validate_rechapcha($captchaResponse) === false) {
+
+            exit;
+        }
+
+        
         $data = [
             'firstname'  => get('firstname'),
             'lastname'  => get('lastname'),
             'email' => get('email'),
             'text'  => get('text'),
+
         
            
         ];
@@ -37,40 +113,14 @@ return function($kirby, $pages, $page) {
           
         ];
 
-        $captchaInvalid = true;
 
-        // Validate captcha
-        $curlx = curl_init();
-        curl_setopt($curlx, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
-        curl_setopt($curlx, CURLOPT_HEADER, 0);
-        curl_setopt($curlx, CURLOPT_RETURNTRANSFER, 1); 
-        curl_setopt($curlx, CURLOPT_POST, 1);
 
-        $post_data = 
-        [
-            'secret' => '6LdE7fUiAAAAAJsKuqbVi6E2E5p9XNxk5q3dt_4c', //<--- your reCaptcha secret key
-            'response' => $_POST['g-recaptcha-response']
-        ];
-        curl_setopt($curlx, CURLOPT_POSTFIELDS, $post_data);
-        $resp = json_decode(curl_exec($curlx));
-        curl_close($curlx);
-
-        if ($resp->success) 
-        {
-          $captchaInvalid = false;
-        } else {
-            $captchaInvalid = true;    
-        }
-        $invalid = invalid($data, $rules, $messages);
 
         // some of the data is invalid
-        if ($captchaInvalid && $invalid) {
-            $alert = "";
-            if ($invalid) {
-                $alert = $invalid;
-            } else {
-                $alert = "You failed to check that you are not a robot";
-            }
+        if($invalid = invalid($data, $rules, $messages)) {
+            $alert = $invalid;
+
+            // the data is fine, let's send the email
         } else {
             try {
                 
@@ -107,3 +157,14 @@ return function($kirby, $pages, $page) {
         'success' => $success ?? false
     ];
 };
+
+
+
+
+
+
+
+
+
+
+
